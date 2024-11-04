521ad902947e3ac6bb9c17a25f2a0a2a9243911d
Fred Morrison <frederick.morrison@outlook.com>
2024-11-04 12:58:52 -0700

    show blank window, scaled by PyAutoGUI

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import platform
 import sys
 #
-import tkinter
+import tkinter as tk
 from tkinter import *
+#
+from settings import Settings
+
+settings = Settings(6.0 / 10)
+
+
+def center_window(window):
+    """Center the Tkinter window in the middle of the screen using scale factor."""
+    window.update_idletasks()
+    # print(f'{window.winfo_width()=}, {window.winfo_height()=}')
+    # print(f'{window.winfo_screenwidth()=}, {window.winfo_screenheight()=}')
+    width = settings.scaled_width
+    height = settings.scaled_height
+
+    screen_width = settings.device_width
+    screen_height = settings.device_height
+    print(f'{screen_width=}, {screen_height=}')
+
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window_geometry = f'{width}x{height}+{x}+{y}'
+    print(f'{window_geometry=}')
+    window.geometry(window_geometry)
 
 
 def get_python_version() -> str:
@@ -10,7 +33,7 @@ def get_python_version() -> str:
 
 
 def get_tkinter_version() -> str:
-    return f'{tkinter.TkVersion}'
+    return f'{tk.TkVersion}'
 
 
 def main():
@@ -20,6 +43,12 @@ def main():
     msg = f'Tkinter version: {get_tkinter_version()}'
     print(msg)
 
-    from settings import Settings
+    root = tk.Tk()
+    title = f'Example using Tkinter {get_tkinter_version()} and python {get_python_version()}'
+    root.title(title)
+    center_window(root)
+    root.mainloop()
 
-    settings = Settings(6.0 / 10)
+
+if __name__ == '__main__':
+    main()
