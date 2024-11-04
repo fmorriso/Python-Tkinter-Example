8db091b613f79927f9d25b1ce82633938f64a97c
Fred Morrison <frederick.morrison@outlook.com>
2024-11-04 15:23:02 -0700

    modify scaling to use built-in Tkinter features instead of PyAutoGUI

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ import tkinter as tk
 from tkinter import *
 # from ctypes import windll
 #
-from settings import Settings
 
-settings = Settings(6.4 / 10)
-
-
-def center_window(window):
+def center_window(window, pct: float):
     """Center the Tkinter window in the middle of the screen using scale factor."""
     window.update_idletasks()
 
-    x = (settings.device_width - settings.scaled_width) // 2
-    y = (settings.device_height - settings.scaled_height) // 2
-    window_geometry = f'{settings.scaled_width}x{settings.scaled_height}+{x}+{y}'
-    # print(f'{window_geometry=}')
+    device_width = window.winfo_screenwidth()
+    scaled_width = int(window.winfo_screenwidth() * pct / 100) * 100
+
+    device_height = window.winfo_screenheight()
+    scaled_height = int(window.winfo_screenheight() * pct / 100) * 100
+
+    x = (device_width - scaled_width) // 2
+    y = (device_height - scaled_height) // 2
+    window_geometry = f'{scaled_width}x{scaled_height}+{x}+{y}'
+    print(f'{window_geometry=}')
     window.geometry(window_geometry)
-    window.resizable(False, False)
+
 
 
 def get_python_version() -> str:
@@ -42,7 +44,8 @@ def main():
     root = tk.Tk()
     title = f'Example using Tkinter {get_tkinter_version()} and python {get_python_version()}'
     root.title(title)
-    center_window(root)
+    center_window(root, 72./100)
+    root.resizable(False, False)
 
     # place a label on the root window
     message = tk.Label(root, text = 'Hello, World!')
