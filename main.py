0c854485bb134b10f426ddf1aa54b50b9b8de602
Fred Morrison <frederick.morrison@outlook.com>
2024-11-04 15:59:19 -0700

    refactor scale_and_center method

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ from tkinter import *
 # from ctypes import windll
 #
 
-def scale_and_center_window(window: tk.Tk, pct: float) -> None:
+def scale_and_center_window(window: tk.Tk, pct: float = 0.75, multiple_of: int = 100) -> None:
     """
-    Scale and center the Tkinter window in the middle of the screen using the specified percentage.
+    Scale and center the Tkinter window in the middle of the screen using the specified percentage,
+    rounded to the specified multiple of value.
     :param window: The Tkinter window to size and center.
     :param pct: Percentage of the available device width and height to use (between 0 and 1).
+    :param multiple_of: The multiple of value that the scaled window should be rounded to.
     """
     window.update_idletasks()
 
     device_width = window.winfo_screenwidth()
-    scaled_width = int(window.winfo_screenwidth() * pct / 100) * 100
+    scaled_width = int(device_width * pct / multiple_of) * multiple_of
 
     device_height = window.winfo_screenheight()
-    scaled_height = int(window.winfo_screenheight() * pct / 100) * 100
+    scaled_height = int(device_height * pct / multiple_of) * multiple_of
 
-    x = (device_width - scaled_width) // 2
-    y = (device_height - scaled_height) // 2
+    x = int((device_width - scaled_width)) // 2
+    y = int((device_height - scaled_height)) // 2
 
     window_geometry = f'{scaled_width}x{scaled_height}+{x}+{y}'
     print(f'{window_geometry=}')
@@ -50,7 +52,7 @@ def main():
     print(f'{type(root)=}')
     title = f'Example using Tkinter {get_tkinter_version()} and python {get_python_version()}'
     root.title(title)
-    scale_and_center_window(root, 72. / 100)
+    scale_and_center_window(root, 4./5, multiple_of = 32)
     root.resizable(False, False)
 
     # use a different icon instead of the default
